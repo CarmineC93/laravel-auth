dd3b2abe9c69b249773daeb416d7289139eb4e58
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-10 16:24:44 +0100

    add data type and table in migration function up()

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +13,9 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i > 10; $i++) {
+        }
     }
 }
