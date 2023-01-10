0b90bc77a5ce6958094d66a5deef8a60dd728058
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-10 17:33:32 +0100

    not working link to page index

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -61,6 +61,14 @@
                                 </a>
                             </li>
 
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.projects.index' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.projects.index') }}">
+                                    <i class="fa-solid fa-list"></i>
+                                    Projects
+                                </a>
+                            </li>
+
 
                         </ul>
                     </div>
