1eabaf21b23d458251ba633e9ba5cc2a029f388c
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-10 15:23:10 +0100

    create route index admin

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    // funzione che rimanda alla pagina index
+    public function index()
+    {
+    }
+}
