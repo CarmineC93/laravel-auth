a676477b09d04ca6134c40af0afe03431220eaa4
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-10 16:30:26 +0100

    fill projectSeeder with slug use Illuminate\Support\Str;

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -16,6 +18,11 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker)
     {
         for ($i = 0; $i > 10; $i++) {
+            $project = new Project();
+            $project->title = $faker->sentence(3);
+            $project->description = $faker->text(400);
+            $project->slug = Str::slug($project->title, '-');
+            $project->save();
         }
     }
 }
