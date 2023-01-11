0d8582fd3134dd92622e623d283bf8214f95838c
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-11 15:05:05 +0100

    add create.blade.php file with a form

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <h2 class="text-center mt-3">Crea un nuovo progetto</h2>
+        <div class="row justify-content-center">
+            <div class="col-8">
+
+                <div class="form-group mb-3">
+                    <label for="title">Titolo</label>
+                    <input type="text" id="title" name="title"
+                        class="form-control @error('title')
+                        is-invalid
+                        @enderror"
+                        value="{{ old('title') }}">
+                    {{-- @error('title')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror --}}
+                </div>
+
+                <div class="form-group mb-3">
+                    <label for="content">Descrizione</label>
+                    <textarea name="content" id="content" rows="10"
+                        class="form-control @error('content')
+                        is-invalid
+                        @enderror">{{ old('content') }}</textarea>
+                    {{-- @error('content')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror --}}
+                </div>
+
+                <button class="btn btn-success" type="submit">Salva</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
